5ed9bdd8d8f658e95d7095d23fa7050e230ba7c9
c3k4ah <dominickp20.aps1b@gmail.com>
2023-09-13 12:55:19 +0300

    [ADD] template creator

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 /*
 Copyright © 2023 DOMINICK Randriamanantena Grégoire dominick.randriamanantena@gmail.com
-
 */
 package main
 
-import "cleancode/cmd"
+import (
+	"cleancode/cmd"
+	// templates "cleancode/code_templates/data"
+)
 
 func main() {
 	cmd.Execute()
+
+	// templates.CreateDataRepository("User")
+	// templates.CreateDataModel("User")
+
 }
